ee408d00fcf4fd744ec48f31d70c82466c7a113d
saitejakola <saiteja.01999@gmail.com>
2024-12-22 15:33:42 +0530

    Handling close with esc btn

--- DOM/Modal Window/script.js
+++ DOM/Modal Window/script.js
@@ -3,8 +3,6 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const showModal = document.querySelectorAll('.show-modal')
-console.log(showModal);
-
 
 const handleClick = () => {
     modal.classList.remove('hidden');
@@ -14,8 +12,14 @@ const handleClose = () => {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 }
+const handleCloseEsc = (event) => {
+    if (event.key === 'Escape' && !modal.classList.contains('hidden'))
+        handleClose();
+}
+//handling different events
 for (let ele of showModal) {
     ele.addEventListener('click', handleClick);
 }
 btnCloseModal.addEventListener('click', handleClose);
 overlay.addEventListener('click', handleClose);
+document.addEventListener('keydown', handleCloseEsc);
